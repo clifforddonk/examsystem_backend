17c17831f9e591e09f4c5765922f667400fc9f25
clifforddonk <clifforddonk@gmail.com>
2025-03-15 12:05:52 +0000

    fetch by category

--- src/main/java/com/exam/exambackend/repository/QuestionRepository.java
+++ src/main/java/com/exam/exambackend/repository/QuestionRepository.java
@@ -3,5 +3,9 @@ package com.exam.exambackend.repository;
 import com.exam.exambackend.model.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends MongoRepository<Question, String> {
+    // Method to find questions by category
+    List<Question> findByCategory(String category);
 }
